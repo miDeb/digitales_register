a5af5022349ea58030a712b219b3815fdd3c3013
Michael <michael.debertol@gmail.com>
2019-05-01 19:21:26 +0200

    end the app if the back button is pressed

--- lib/ui/login_page_content.dart
+++ lib/ui/login_page_content.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../container/login_page.dart';
 import 'no_internet.dart';
@@ -24,90 +25,97 @@ class _LoginPageContentState extends State<LoginPageContent> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-        automaticallyImplyLeading: false,
-      ),
-      body: widget.vm.noInternet
-          ? Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  NoInternet(),
-                  SizedBox(
-                    height: 25,
+    return WillPopScope(
+      onWillPop: (){
+        SystemNavigator.pop();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Login"),
+          automaticallyImplyLeading: false,
+        ),
+        body: widget.vm.noInternet
+            ? Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    NoInternet(),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    RaisedButton(
+                      child: Text("Nochmal versuchen"),
+                      onPressed: () => widget.vm.onReload(),
+                    ),
+                  ],
+                ),
+              )
+            : Stack(
+                children: [
+                  Center(
+                    child: ListView(
+                      physics: AlwaysScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Column(children: [
+                            TextField(
+                              decoration:
+                                  InputDecoration(labelText: 'Username'),
+                              controller: _usernameController,
+                              enabled: !widget.vm.loading,
+                            ),
+                            TextField(
+                              decoration:
+                                  InputDecoration(labelText: 'Passwort'),
+                              controller: _passwordController,
+                              obscureText: true,
+                              enabled: !widget.vm.loading,
+                            ),
+                            RaisedButton(
+                              onPressed: widget.vm.loading
+                                  ? null
+                                  : () {
+                                      widget.vm.setSafeMode(safeMode);
+                                      widget.vm.onLogin(
+                                          _usernameController.value.text,
+                                          _passwordController.value.text);
+                                    },
+                              child: Text('Login'),
+                            ),
+                            Divider(),
+                          ]),
+                        ),
+                        SwitchListTile(
+                          title: Text("Angemeldet bleiben"),
+                          value: !safeMode,
+                          onChanged: widget.vm.loading
+                              ? null
+                              : (bool value) {
+                                  setState(() {
+                                    safeMode = !value;
+                                  });
+                                },
+                        ),
+                        Center(
+                          child: widget.vm.error?.isNotEmpty == true
+                              ? Text(
+                                  widget.vm.error,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .body1
+                                      .copyWith(color: Colors.red),
+                                )
+                              : SizedBox(),
+                        ),
+                      ],
+                    ),
                   ),
-                  RaisedButton(
-                    child: Text("Nochmal versuchen"),
-                    onPressed: () => widget.vm.onReload(),
-                  ),
+                  widget.vm.loading ? LinearProgressIndicator() : SizedBox(),
                 ],
               ),
-            )
-          : Stack(
-              children: [
-                Center(
-                  child: ListView(
-                    physics: AlwaysScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Column(children: [
-                          TextField(
-                            decoration: InputDecoration(labelText: 'Username'),
-                            controller: _usernameController,
-                            enabled: !widget.vm.loading,
-                          ),
-                          TextField(
-                            decoration: InputDecoration(labelText: 'Passwort'),
-                            controller: _passwordController,
-                            obscureText: true,
-                            enabled: !widget.vm.loading,
-                          ),
-                          RaisedButton(
-                            onPressed: widget.vm.loading
-                                ? null
-                                : () {
-                                    widget.vm.setSafeMode(safeMode);
-                                    widget.vm.onLogin(
-                                        _usernameController.value.text,
-                                        _passwordController.value.text);
-                                  },
-                            child: Text('Login'),
-                          ),
-                          Divider(),
-                        ]),
-                      ),
-                      SwitchListTile(
-                        title: Text("Angemeldet bleiben"),
-                        value: !safeMode,
-                        onChanged: widget.vm.loading
-                            ? null
-                            : (bool value) {
-                                setState(() {
-                                  safeMode = !value;
-                                });
-                              },
-                      ),
-                      Center(
-                        child: widget.vm.error?.isNotEmpty == true
-                            ? Text(
-                                widget.vm.error,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .body1
-                                    .copyWith(color: Colors.red),
-                              )
-                            : SizedBox(),
-                      ),
-                    ],
-                  ),
-                ),
-                widget.vm.loading ? LinearProgressIndicator() : SizedBox(),
-              ],
-            ),
+      ),
     );
   }
 }
