9bd8c21ebffffe88d5bf4bbb381fac8107dd1d26
Michael <michael.debertol@gmail.com>
2019-05-08 20:17:30 +0200

    change string for no homeworks

--- lib/ui/days.dart
+++ lib/ui/days.dart
@@ -140,7 +140,7 @@ class DayWidget extends StatelessWidget {
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
-                          "Noch keine Aufgaben",
+                          "Nichts eingetragen",
                           style: Theme.of(context).textTheme.subhead,
                         ),
                       ),
