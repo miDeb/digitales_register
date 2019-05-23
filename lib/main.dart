6b5f7db6604dfeb9a223cf04165e515aeeb92d31
Michael <michael.debertol@gmail.com>
2019-05-23 18:58:31 +0200

    remove unneeded material white color

--- lib/main.dart
+++ lib/main.dart
@@ -27,22 +27,6 @@ void main() {
   run();
 }
 
-const MaterialColor white = const MaterialColor(
-  0xFFFFFFFF,
-  const <int, Color>{
-    50: const Color(0xFFFFFFFF),
-    100: const Color(0xFFFFFFFF),
-    200: const Color(0xFFFFFFFF),
-    300: const Color(0xFFFFFFFF),
-    400: const Color(0xFFFFFFFF),
-    500: const Color(0xFFFFFFFF),
-    600: const Color(0xFFFFFFFF),
-    700: const Color(0xFFFFFFFF),
-    800: const Color(0xFFFFFFFF),
-    900: const Color(0xFFFFFFFF),
-  },
-);
-
 final initialState = AppState((builder) {
   builder
     ..dayState = (DayStateBuilder()
