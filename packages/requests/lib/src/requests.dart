3a5dc3cbc43af7fe91635514debac17b35e2ee46
Michael <michael.debertol@gmail.com>
2019-09-13 18:24:29 +0200

    Revert "make Request's methods non-static"
    This reverts commit c778359ee09909856f0e8d0fd585495dac827e53.

--- packages/requests/lib/src/requests.dart
+++ packages/requests/lib/src/requests.dart
@@ -10,9 +10,9 @@ import 'event.dart';
 final Logger log = new Logger('requests');
 
 class Requests {
-  Requests();
+  const Requests();
 
-  final Event onError = Event();
+  static final Event onError = Event();
   static const String HTTP_METHOD_GET = "get";
   static const String HTTP_METHOD_PUT = "put";
   static const String HTTP_METHOD_PATCH = "patch";
@@ -21,7 +21,7 @@ class Requests {
   static const String HTTP_METHOD_HEAD = "head";
   static const int DEFAULT_TIMEOUT_SECONDS = 10;
 
-  Set _cookiesKeysToIgnore = Set.from([
+  static Set _cookiesKeysToIgnore = Set.from([
     "SameSite",
     "Path",
     "Domain",
@@ -31,7 +31,7 @@ class Requests {
     "HttpOnly"
   ]);
 
-  Map<String, String> _extractResponseCookies(responseHeaders) {
+  static Map<String, String> _extractResponseCookies(responseHeaders) {
     Map<String, String> cookies = {};
     for (var key in responseHeaders.keys) {
       if (Common.equalsIgnoreCase(key, 'set-cookie')) {
@@ -49,7 +49,7 @@ class Requests {
     return cookies;
   }
 
-  Future<Map> _constructRequestHeaders(
+  static Future<Map> _constructRequestHeaders(
       String hostname, Map<String, String> customHeaders) async {
     var cookies = await getStoredCookies(hostname);
     String cookie =
@@ -63,7 +63,7 @@ class Requests {
     return requestHeaders;
   }
 
-  Future<Map<String, String>> getStoredCookies(String hostname) async {
+  static Future<Map<String, String>> getStoredCookies(String hostname) async {
     try {
       String hostnameHash = Common.hashStringSHA256(hostname);
       String cookiesJson = await Common.storageGet('cookies-$hostnameHash');
@@ -76,23 +76,24 @@ class Requests {
     }
   }
 
-  Future setStoredCookies(String hostname, Map<String, String> cookies) async {
+  static Future setStoredCookies(
+      String hostname, Map<String, String> cookies) async {
     String hostnameHash = Common.hashStringSHA256(hostname);
     String cookiesJson = Common.toJson(cookies);
     await Common.storageSet('cookies-$hostnameHash', cookiesJson);
   }
 
-  Future clearStoredCookies(String hostname) async {
+  static Future clearStoredCookies(String hostname) async {
     String hostnameHash = Common.hashStringSHA256(hostname);
     await Common.storageSet('cookies-$hostnameHash', null);
   }
 
-  String getHostname(String url) {
+  static String getHostname(String url) {
     var uri = Uri.parse(url);
     return uri.host;
   }
 
-  Future<dynamic> _handleHttpResponse(String url, String hostname,
+  static Future<dynamic> _handleHttpResponse(String url, String hostname,
       http.Response response, bool json, bool persistCookies) async {
     int statusCode = response.statusCode;
     dynamic responseBody = "";
@@ -135,7 +136,7 @@ class Requests {
     return responseBody;
   }
 
-  Future<dynamic> head(String url,
+  static Future<dynamic> head(String url,
       {headers,
       timeoutSeconds = DEFAULT_TIMEOUT_SECONDS,
       json = false,
@@ -147,7 +148,7 @@ class Requests {
         persistCookies: persistCookies);
   }
 
-  Future<dynamic> get(String url,
+  static Future<dynamic> get(String url,
       {headers,
       timeoutSeconds = DEFAULT_TIMEOUT_SECONDS,
       json = false,
@@ -159,7 +160,7 @@ class Requests {
         persistCookies: persistCookies);
   }
 
-  Future<dynamic> patch(String url,
+  static Future<dynamic> patch(String url,
       {headers,
       timeoutSeconds = DEFAULT_TIMEOUT_SECONDS,
       json = false,
@@ -171,7 +172,7 @@ class Requests {
         persistCookies: persistCookies);
   }
 
-  Future<dynamic> delete(String url,
+  static Future<dynamic> delete(String url,
       {headers,
       timeoutSeconds = DEFAULT_TIMEOUT_SECONDS,
       json = false,
@@ -183,7 +184,7 @@ class Requests {
         persistCookies: persistCookies);
   }
 
-  Future<dynamic> post(String url,
+  static Future<dynamic> post(String url,
       {body,
       headers,
       timeoutSeconds = DEFAULT_TIMEOUT_SECONDS,
@@ -197,7 +198,7 @@ class Requests {
         persistCookies: persistCookies);
   }
 
-  Future<dynamic> put(String url,
+  static Future<dynamic> put(String url,
       {body,
       headers,
       timeoutSeconds = DEFAULT_TIMEOUT_SECONDS,
@@ -211,7 +212,7 @@ class Requests {
         persistCookies: persistCookies);
   }
 
-  Future<dynamic> _httpRequest(String method, String url,
+  static Future<dynamic> _httpRequest(String method, String url,
       {body,
       headers,
       timeoutSeconds = DEFAULT_TIMEOUT_SECONDS,
--- packages/requests/test/requests_test.dart
+++ packages/requests/test/requests_test.dart
@@ -1,48 +1,44 @@
-import 'package:dr/requests/lib/requests.dart';
+import 'package:requests/requests.dart';
 import 'package:test_api/test_api.dart';
 
 void main() {
   group('A group of tests', () {
-    final requests = Requests();
     test('plain http get', () async {
-      String body = await requests.get("https://google.com");
+      String body = await Requests.get("https://google.com");
       expect(body, isNotNull);
-    }, skip: "fails");
+    });
 
     test('json http get list of objects', () async {
-      dynamic body = await requests
-          .get("https://jsonplaceholder.typicode.com/posts", json: true);
+      dynamic body = await Requests.get("https://jsonplaceholder.typicode.com/posts", json: true);
       expect(body, isNotNull);
       expect(body, isList);
     });
 
     test('json http post', () async {
-      await requests.post("https://jsonplaceholder.typicode.com/posts", body: {
+      await Requests.post("https://jsonplaceholder.typicode.com/posts", body: {
         "userId": 10,
         "id": 91,
         "title": "aut amet sed",
-        "body":
-            "libero voluptate eveniet aperiam sed\nsunt placeat suscipit molestias\nsimilique fugit nam natus\nexpedita consequatur consequatur dolores quia eos et placeat",
+        "body": "libero voluptate eveniet aperiam sed\nsunt placeat suscipit molestias\nsimilique fugit nam natus\nexpedita consequatur consequatur dolores quia eos et placeat",
       });
     });
 
     test('json http get object', () async {
-      dynamic body = await requests
-          .get("https://jsonplaceholder.typicode.com/posts/1", json: true);
+      dynamic body = await Requests.get("https://jsonplaceholder.typicode.com/posts/1", json: true);
       expect(body, isNotNull);
       expect(body, isMap);
     });
 
     test('remove cookies', () async {
       String url = "https://jsonplaceholder.typicode.com/posts/1";
-      String hostname = requests.getHostname(url);
+      String hostname = Requests.getHostname(url);
       expect("jsonplaceholder.typicode.com", hostname);
-      await requests.clearStoredCookies(hostname);
-      await requests.setStoredCookies(hostname, {'session': 'bla'});
-      var cookies = await requests.getStoredCookies(hostname);
+      await Requests.clearStoredCookies(hostname);
+      await Requests.setStoredCookies(hostname, {'session': 'bla'});
+      var cookies = await Requests.getStoredCookies(hostname);
       expect(cookies.keys.length, 1);
-      await requests.clearStoredCookies(hostname);
-      cookies = await requests.getStoredCookies(hostname);
+      await Requests.clearStoredCookies(hostname);
+      cookies = await Requests.getStoredCookies(hostname);
       expect(cookies.keys.length, 0);
     });
   });
