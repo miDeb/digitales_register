3ba164e4bf0069f06e207de8f8e9f163dc4c37a3
Michael <michael.debertol@gmail.com>
2019-11-03 18:19:28 +0100

    grades chart: refactor domainAxis ticks

--- lib/ui/grades_chart.dart
+++ lib/ui/grades_chart.dart
@@ -1,5 +1,6 @@
-import 'package:charts_flutter/flutter.dart' as chart;
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../app_state.dart';
 import '../container/grades_chart_container.dart';
@@ -12,13 +13,13 @@ final colors = List.of(Colors.primaries)
 
 class GradesChart extends StatelessWidget {
   final GradesChartViewModel vm;
-  final List<chart.Series<MapEntry<DateTime, int>, DateTime>> grades;
+  final List<charts.Series<MapEntry<DateTime, int>, DateTime>> grades;
 
   GradesChart({Key key, this.vm})
       : grades = convert(vm.graphs),
         super(key: key);
 
-  static List<chart.Series<MapEntry<DateTime, int>, DateTime>> convert(
+  static List<charts.Series<MapEntry<DateTime, int>, DateTime>> convert(
       Map<SubjectGrades, SubjectGraphConfig> data) {
     int count = 0;
     return data.entries
@@ -29,8 +30,8 @@ class GradesChart extends StatelessWidget {
             final color = Color(entry.value.color) ?? colors[count++];
             return strokeWidth == 0
                 ? null
-                : chart.Series<MapEntry<DateTime, int>, DateTime>(
-                    colorFn: (_, __) => chart.Color(
+                : charts.Series<MapEntry<DateTime, int>, DateTime>(
+                    colorFn: (_, __) => charts.Color(
                         r: color.red, g: color.green, b: color.blue),
                     domainFn: (grade, _) => grade.key,
                     measureFn: (grade, _) => grade.value / 100,
@@ -44,31 +45,82 @@ class GradesChart extends StatelessWidget {
         .toList();
   }
 
+  List<charts.TickSpec<DateTime>> createDomainAxisTags(Locale locale) {
+    var firstMonth = grades[0].data.first.key;
+    var lastMonth = grades[0].data.last.key;
+    for (final sub in grades) {
+      if (sub.data.isEmpty) continue;
+      final firstSubjectDate = sub.data.last.key;
+      final lastSubjectDate = sub.data.first.key;
+      if (firstSubjectDate.isBefore(firstMonth)) {
+        firstMonth = firstSubjectDate;
+      }
+      if (lastSubjectDate.isAfter(lastMonth)) {
+        lastMonth = lastSubjectDate;
+      }
+    }
+    final dates = [
+      DateTime(firstMonth.year, firstMonth.month,
+          firstMonth.day < 15 ? 15 : firstMonth.day)
+    ];
+    while (true) {
+      final newDate = DateTime(dates.last.year, dates.last.month + 1, 15);
+      if (lastMonth.isBefore(newDate)) break;
+      dates.add(newDate);
+    }
+    if (dates.last.month != lastMonth.month) dates.add(lastMonth);
+    if (dates.length == 1) {
+      return [firstMonth, lastMonth].map((date) {
+        return charts.TickSpec(
+          date,
+          label: DateFormat.MMMMd(
+            locale.toLanguageTag(),
+          ).format(date),
+        );
+      }).toList();
+    } else {
+      return dates.map((date) {
+        return charts.TickSpec(
+          date,
+          label: DateFormat.MMM(
+            locale.toLanguageTag(),
+          ).format(date),
+        );
+      }).toList();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Hero(
       tag: 1337,
       child: maybeWrap(
           Center(
-            child: chart.TimeSeriesChart(
+            child: charts.TimeSeriesChart(
               grades,
               animate: false,
-              primaryMeasureAxis: chart.NumericAxisSpec(
-                tickProviderSpec: chart.StaticNumericTickProviderSpec(
+              primaryMeasureAxis: charts.NumericAxisSpec(
+                tickProviderSpec: charts.StaticNumericTickProviderSpec(
                   [
-                    chart.TickSpec(3),
-                    chart.TickSpec(4),
-                    chart.TickSpec(5),
-                    chart.TickSpec(6),
-                    chart.TickSpec(7),
-                    chart.TickSpec(8),
-                    chart.TickSpec(9),
-                    chart.TickSpec(10),
+                    charts.TickSpec(3),
+                    charts.TickSpec(4),
+                    charts.TickSpec(5),
+                    charts.TickSpec(6),
+                    charts.TickSpec(7),
+                    charts.TickSpec(8),
+                    charts.TickSpec(9),
+                    charts.TickSpec(10),
                   ],
                 ),
               ),
               defaultInteractions: false,
-              domainAxis: new chart.EndPointsTimeAxisSpec(),
+              domainAxis: new charts.DateTimeAxisSpec(
+                tickProviderSpec: charts.StaticDateTimeTickProviderSpec(
+                  createDomainAxisTags(
+                    Localizations.localeOf(context),
+                  ),
+                ),
+              ),
             ),
           ),
           !vm.isFullScreen, (widget) {
