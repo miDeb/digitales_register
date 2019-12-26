da51d35c181d7c6d48021458361af7901c09a2cc
Michael <michael.debertol@gmail.com>
2019-12-26 21:51:07 +0100

    basic tests

--- test/no_internet.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dr/wrapper.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-main() {
-  test('no internet detection', () async {
-    print(await Wrapper().noInternet);
-  });
-}
---
+++ test/parse_test.dart
@@ -0,0 +1,79 @@
+import 'package:built_redux/built_redux.dart';
+import 'package:dr/actions/app_actions.dart';
+import 'package:dr/actions/dashboard_actions.dart';
+import 'package:dr/actions/grades_actions.dart';
+import 'package:dr/app_state.dart';
+import 'package:dr/middleware/middleware.dart';
+import 'package:dr/reducer/reducer.dart';
+import 'package:dr/requests/absences.dart';
+import 'package:dr/requests/calendar.dart';
+import 'package:dr/requests/dashboard.dart';
+import 'package:dr/requests/grades_first.dart' as first;
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  group('parse test', () {
+    final store = Store<AppState, AppStateBuilder, AppActions>(
+      appReducerBuilder.build(),
+      AppState(),
+      AppActions(),
+      middleware: middleware,
+    );
+    test('parse absences', () {
+      expect(store.state.absencesState == null, true);
+      store.actions.absencesActions.loaded(absences);
+      expect(store.state.absencesState != null, true);
+      expect(store.state.absencesState.absences.length, 6);
+    });
+    test('parse dashboard', () {
+      expect(store.state.dashboardState.allDays, null);
+      store.actions.dashboardActions.loaded(DaysLoadedPayload(
+        (b) => b
+          ..data = fullSchoolYear
+          ..future = false
+          ..markNewOrChangedEntries = true,
+      ));
+      expect(store.state.dashboardState.allDays.length, 177);
+    });
+    test('parse calendar', () {
+      expect(store.state.calendarState.days.isEmpty, true);
+      store.actions.calendarActions.loaded(calendar1);
+      expect(store.state.calendarState.days.length, 5);
+      store.actions.calendarActions.loaded(calendar2);
+      expect(store.state.calendarState.days.length, 10);
+      store.actions.calendarActions.loaded(calendar2);
+      expect(store.state.calendarState.days.length, 10);
+    });
+    test('parse grades', () {
+      expect(store.state.gradesState.hasGrades, false);
+      expect(store.state.gradesState.subjects.isEmpty, true);
+      store.actions.gradesActions.loaded(
+        SubjectsLoadedPayload(
+          (b) => b
+            ..data = first.all
+            ..semester = Semester.first.toBuilder(),
+        ),
+      );
+      expect(store.state.gradesState.subjects.length, 13);
+      expect(store.state.gradesState.hasGrades, true);
+      expect(
+        store.state.gradesState.subjects[0].detailEntries(Semester.first),
+        null,
+      );
+      store.actions.gradesActions.detailsLoaded(
+        SubjectDetailLoadedPayload(
+          (b) => b
+            ..data = first.pe
+            ..subject = store.state.gradesState.subjects[0].toBuilder()
+            ..semester = Semester.first.toBuilder(),
+        ),
+      );
+      expect(
+        store.state.gradesState.subjects[0]
+            .detailEntries(Semester.first)
+            .length,
+        6,
+      );
+    });
+  });
+}
