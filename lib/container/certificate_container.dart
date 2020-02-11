e1f893f89b5ada46558dea8c014c2de1b24a57c8
Michael <michael.debertol@gmail.com>
2020-02-11 18:39:41 +0100

    show a no internet screen in the certificate view

--- lib/container/certificate_container.dart
+++ lib/container/certificate_container.dart
@@ -1,21 +1,27 @@
-import 'package:dr/ui/certificate.dart';
-
-import '../actions/app_actions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_built_redux/flutter_built_redux.dart';
 
+import '../actions/app_actions.dart';
 import '../app_state.dart';
+import '../ui/certificate.dart';
 
 class CertificateContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StoreConnection<AppState, AppActions, String>(
+    return StoreConnection<AppState, AppActions, CertificateViewModel>(
       connect: (state) {
-        return state.certificateState.html;
+        return CertificateViewModel(state.certificateState.html, state.noInternet);
       },
       builder: (context, vm, actions) {
         return Certificate(vm: vm);
       },
     );
   }
+}
+
+class CertificateViewModel {
+  final String html;
+  final bool noInternet;
+
+  CertificateViewModel(this.html, this.noInternet);
 }
