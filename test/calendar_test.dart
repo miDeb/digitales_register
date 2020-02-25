0e82220f2e875436b4ce4b339aece0792d9be626
Michael <michael.debertol@gmail.com>
2020-02-25 15:42:02 +0100

    remove useless printout

--- test/calendar_test.dart
+++ test/calendar_test.dart
@@ -3,7 +3,6 @@ import '../lib/ui/calendar.dart';
 
 main() {
   test('to monday; page of', () {
-    print(DateTime.parse("2019-04-15"));
     expect(pageOf(mondayOf(124564)), 124564);
     final date = DateTime.utc(2019, 4, 15);
     expect(mondayOf(pageOf(date)), date);
