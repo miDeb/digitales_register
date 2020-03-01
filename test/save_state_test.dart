7202053796602c648782f4e3ba657c8021b4a0f4
Michael <michael.debertol@gmail.com>
2020-03-01 21:03:28 +0100

    add more save state tests * make sre no data is saved * make sure data is deleted on logout

--- test/save_state_test.dart
+++ test/save_state_test.dart
@@ -1,11 +1,14 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:built_redux/built_redux.dart';
 import 'package:dr/actions/app_actions.dart';
+import 'package:dr/actions/login_actions.dart';
 import 'package:dr/app_state.dart';
 import 'package:dr/linux.dart';
 import 'package:dr/middleware/middleware.dart';
 import 'package:dr/reducer/reducer.dart';
+import 'package:dr/serializers.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 main() {
@@ -62,12 +65,104 @@ main() {
     store.actions.saveState();
 
     // the state should be saved immediately
-    await Future.delayed(Duration(milliseconds: 250));
+    await Future.delayed(Duration(milliseconds: 100));
+    expect(
+      await saveFile.exists(),
+      true,
+    );
+    expect(
+        serializers.deserialize(json.decode(await saveFile.readAsString()))
+            is AppState,
+        true);
+
+    saveFile.deleteSync();
+  });
+  test('state is not saved when it is disabled', () async {
+    final username = "test_username2";
+    final store = Store<AppState, AppStateBuilder, AppActions>(
+      appReducerBuilder.build(),
+      AppState(
+        (b) {
+          b.loginState
+            ..loggedIn = true
+            ..username = username;
+          b.settingsState.noDataSaving = true;
+        },
+      ),
+      AppActions(),
+      middleware: middleware,
+    );
+    Directory("linux/appData").createSync(recursive: true);
+
+    final saveFile = File(
+        "${(await getApplicationDocumentsDirectory()).path}/app_state_${username.hashCode}.json");
+    // make sure the file does not already exist
+    if (saveFile.existsSync()) saveFile.deleteSync();
+
+    store.actions.saveState();
+
+    // the state should be saved immediately
+    await Future.delayed(Duration(milliseconds: 100));
     expect(
       await saveFile.exists(),
       true,
     );
 
+    expect(
+        serializers.deserialize(json.decode(await saveFile.readAsString()))
+            is SettingsState,
+        true);
+
+    saveFile.deleteSync();
+  });
+  test('state is deleted on logout when it is disabled', () async {
+    final username = "test_username3";
+    final store = Store<AppState, AppStateBuilder, AppActions>(
+      appReducerBuilder.build(),
+      AppState(
+        (b) {
+          b.loginState
+            ..loggedIn = true
+            ..username = username;
+          b.settingsState.deleteDataOnLogout = true;
+        },
+      ),
+      AppActions(),
+      middleware: middleware,
+    );
+    Directory("linux/appData").createSync(recursive: true);
+
+    final saveFile = File(
+        "${(await getApplicationDocumentsDirectory()).path}/app_state_${username.hashCode}.json");
+    // make sure the file does not already exist
+    if (saveFile.existsSync()) saveFile.deleteSync();
+
+    store.actions.saveState();
+
+    // the state should be saved immediately
+    await Future.delayed(Duration(milliseconds: 100));
+    expect(
+      await saveFile.exists(),
+      true,
+    );
+
+    expect(
+        serializers.deserialize(json.decode(await saveFile.readAsString()))
+            is AppState,
+        true);
+    store.actions.loginActions.logout(
+      LogoutPayload(
+        (b) => b
+          ..hard = true
+          ..forced = true,
+      ),
+    );
+    await Future.delayed(Duration(milliseconds: 100));
+
+    expect(
+        serializers.deserialize(json.decode(saveFile.readAsStringSync()))
+            is SettingsState,
+        true);
     saveFile.deleteSync();
   });
 }
