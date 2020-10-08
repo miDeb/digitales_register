704c3e65b4fa6b2f240fd7c93dbbf39f2032ed61
Michael Debertol <michael.debertol@gmail.com>
2020-10-08 09:54:18 -0500

    Convert SortedGradesViewModel to built value
    This avoids rebuilds of the grades widget when it is not needed. That's
    mainly for the performance of the grades chart page, because it can
    trigger statee changes, and rebuilding in the background introduces jank.

--- lib/container/sorted_grades_container.dart
+++ lib/container/sorted_grades_container.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Builder;
 import 'package:flutter_built_redux/flutter_built_redux.dart';
 
 import '../actions/app_actions.dart';
@@ -7,12 +7,17 @@ import '../app_state.dart';
 import '../data.dart';
 import '../ui/sorted_grades_widget.dart';
 
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/built_value.dart';
+
+part 'sorted_grades_container.g.dart';
+
 class SortedGradesContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StoreConnection<AppState, AppActions, SortedGradesViewModel>(
       connect: (state) {
-        return SortedGradesViewModel(state);
+        return SortedGradesViewModel.from(state);
       },
       builder: (context, vm, actions) {
         return SortedGradesWidget(
@@ -35,14 +40,25 @@ class SortedGradesContainer extends StatelessWidget {
 typedef ViewSubjectDetailCallback = void Function(Subject s);
 typedef SetBoolCallback = void Function(bool byType);
 
-class SortedGradesViewModel {
-  final List<Subject> subjects;
-  final Semester semester;
-  final bool sortByType, showCancelled, noInternet;
-  SortedGradesViewModel(AppState state)
-      : subjects = state.gradesState.subjects.toList(),
-        sortByType = state.settingsState.typeSorted,
-        semester = state.gradesState.semester,
-        showCancelled = state.settingsState.showCancelled,
-        noInternet = state.noInternet;
+abstract class SortedGradesViewModel
+    implements Built<SortedGradesViewModel, SortedGradesViewModelBuilder> {
+  BuiltList<Subject> get subjects;
+  Semester get semester;
+  bool get sortByType;
+  bool get showCancelled;
+  bool get noInternet;
+
+  factory SortedGradesViewModel.from(AppState state) {
+    return SortedGradesViewModel((b) => b
+      ..subjects = state.gradesState.subjects.toBuilder()
+      ..sortByType = state.settingsState.typeSorted
+      ..semester = state.gradesState.semester.toBuilder()
+      ..showCancelled = state.settingsState.showCancelled
+      ..noInternet = state.noInternet);
+  }
+
+  SortedGradesViewModel._();
+  factory SortedGradesViewModel(
+          [void Function(SortedGradesViewModelBuilder) updates]) =
+      _$SortedGradesViewModel;
 }
